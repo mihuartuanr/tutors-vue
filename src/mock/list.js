227ac3716532c4ed1468a6ef2372121cc5b8d845
张超 <zhangchaouidesign@163.com>
2018-08-11 09:43:30 +0800

    实战Vue简易项目（6）列表页--demo

--- src/mock/list.js
+++ src/mock/list.js
@@ -4,12 +4,30 @@ export default mock({
   'list|0-50': [
     {
       'approveId': '@id',
+      'approveType': '0',//0:事假；1：年假；2：病假
+      'approveTime': Random.datetime('yyyy-mm-dd mm:ss'),
+      'startTime': Random.datetime('yyyy-mm-dd mm:ss'),
+      'endTime': Random.datetime('yyyy-mm-dd mm:ss'),
+      'approveStatus':0,//0：审批中，1：已同意，2：已拒绝，3：执行中，4：已结束
       'applier': {
         'userId': '@guid',
         'userName': '@cname',
         'sectionId': '@id',
         'sectionName': '@ctitle',
-      }
+      },
+      'taskBases|3': [
+        {
+          'taskStatus': '0',//0：未到达，1：待处理，2已同意，3：已拒绝
+          'taskOrder': '0',
+          'comment': '@cparagraph',
+          'userItem': {
+            'userId': '@guid',
+            'userName': '@cname',
+            'sectionId': '@id',
+            'sectionName': '@ctitle',
+          }
+        }
+      ]
     }
   ]
 })
--- src/views/vacation/index.vue
+++ src/views/vacation/index.vue
@@ -1,10 +1,38 @@
 <template>
-  <div>list view</div>
+  <div class="apply">
+    <div class="controller">
+      <button class="create btn">新建申请</button>
+    </div>
+    <div ref="hacker-list">
+      <ul class="list"></ul>
+    </div>
+    <div style="display:none;">
+      <li id="hacker-item">
+        <h3 class="name"></h3>
+        <p class="city"></p>
+      </li>
+    </div>
+  </div>
 </template>
+<script type="template">
+
+</script>
+
 <script>
+import List from "list.js";
+
 export default {
-  beforeCreate(){
-    this.$store.dispatch('requestApplications');
+  mounted() {
+    const options = {
+        item: "hacker-item",
+        valueNames: ['name', 'city'],
+      },
+      values = [
+        { name: "Jonny", city: "Stockholm" },
+        { name: "Jonas", city: "Berlin" }
+      ];
+
+    var hackerList = new List(this.$refs["hacker-list"], options, values);
   }
-}
+};
 </script>
